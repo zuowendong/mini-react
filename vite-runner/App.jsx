91c9fd4dae0a653ee372690f1b141bd6380cd781
zwd <17326195691@163.com>
2024-01-19 14:25:21 +0800

    批量执行action

--- vite-runner/App.jsx
+++ vite-runner/App.jsx
@@ -5,7 +5,8 @@ function Foo() {
   const [bar, setBar] = React.useState("bar");
   function handleClick() {
     setCount((c) => c + 1);
-    setBar((b) => b + ",bar");
+    // setBar((b) => b + ",bar");
+    setBar("bar,bar");
   }
 
   return (
