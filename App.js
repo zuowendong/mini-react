57b08ce71a59435eb8e87156557e1c4eeacec8d6
zuowendong <17326195691@163.com>
2024-03-23 22:52:48 +0800

    chore: init

--- App.js
+++
@@ -1,6 +0,0 @@
-import React from "./core/react.js";
-
-// const textEl = createTextNode("App");
-const App = React.createElement("div", { id: "app" }, "App");
-
-export default App;
--- vite-runner/main.jsx
+++
@@ -1,5 +0,0 @@
-import React from "./core/react.js";
-import ReactDOM from "./core/react-dom.js";
-import App from "./App.jsx";
-
-ReactDOM.createRoot(document.querySelector("#root")).render(<App></App>);
